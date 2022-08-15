e0bca8c6a83d902406e73647d9118b12a560fa3f
BAKHODIR <97035877+Bakhodir001@users.noreply.github.com>
2022-08-16 00:56:30 +0500

    Add files via upload

---
+++ anywords.py
@@ -0,0 +1,16 @@
+words=[
+  "abadiylik",
+  "abbosxoniylar",
+  "Ajdod",
+  "Barbecue",
+  "Barbaros",
+  "Bangladesh",
+  "Billing",
+  "Calm",
+  "Calling",
+  "Content",
+  "Concept",
+  "Karrupsiya",
+  "Manopoliya",
+  
+]
